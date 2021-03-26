82f501ca74cb61c3c01853469363d8ecad915bbb
Hadar Brayer <hadarbrayer@gmail.com>
2021-03-26 23:11:53 +0300

    updating tests

--- test_Booking_scraper.py
+++ test_Booking_scraper.py
@@ -1,73 +1,53 @@
 import pytest
-import conf as cfg
-import Booking_scraper_main_code as BookScrap
+import Booking_scraper_main_code as booking
+import pandas as pd
+import numpy
 
 
-def test_get_hotels_as_dict_type():
-    """Tests the type of the variable the function 'get_hotels_as_dict' returns"""
+def test_get_hotels_names_df():
+    """tests if the function 'get_hotels_names' returns a pandas DataFrame"""
     # setup
-    test_hotel = BookScrap.HotelsManager(cfg.BOOKING_SEYCHELLES)
-    hotels_dict = test_hotel.get_hotels_as_dict()
-
-    assert type(hotels_dict) == dict
-    for key, value in hotels_dict.items():
-        assert type(key) == str
-        assert type(value) == dict
+    test_hotel = booking.HotelsManager(booking.constants["URLS"]["TESTED_URL"])
+    hotels_names_df = test_hotel.get_hotels_names()
+    assert type(hotels_names_df) == pd.Series
 
 
-def test_get_hotels_as_dict_keys():
-    """Tests if the function 'get_hotels_as_dict' returns a dictionary where the values.keys() are all
-        the parameters names that were extract from web parsing and only them """
+def test_get_hotels_names_object_type():
+    """tests if the function 'get_hotels_names' returns hotels names column as object type"""
+    test_hotel = booking.HotelsManager(booking.constants["URLS"]["TESTED_URL"])
+    hotels_names_df = test_hotel.get_hotels_names()
+    assert hotels_names_df.dtype == object
+
+
+def test_get_most_expensive_tuples():
+    """Tests if the function 'most_expensive' returns a tuple (hotel name, price)"""
     # setup
-    test_hotel = BookScrap.HotelsManager(cfg.BOOKING_SEYCHELLES)
-    hotels_dict = test_hotel.get_hotels_as_dict()
-
-    # verify all hotel params are as expected
-    for constant_key in cfg.ALL_HOTEL_KEYS:
-        if constant_key == cfg.HOTEL_NAME_KEY:
-            continue
-        else:
-            for val in hotels_dict.values():
-                assert constant_key in val.keys()
-                assert len(list(val.keys())) == len(cfg.ALL_HOTEL_KEYS)-1   # 'hotel name' is a master key
-
-
-def test_hotels_number():
-    """Tests if the function 'hotels_number' returns a type int variable and that it greater than the 'minimum hotels'
-        number', which is a slightly lower number of hotels than the urls contain """
-    # setup
-    test_hotel = BookScrap.HotelsManager(cfg.BOOKING_SEYCHELLES)
-    hotels_number = test_hotel.hotels_number()
-
-    assert type(hotels_number) == int
-    assert hotels_number > cfg.MIN_HOTELS_NUMBER
-
-
-def test_get_hotels_names():
-    """tests if the function 'get_hotels_names' returns a list of strings and that the length of the list is
-        above the minimum hotels' number, which is a slightly lower number of hotels than the urls contain"""
-    # setup
-    test_hotel = BookScrap.HotelsManager(cfg.BOOKING_SEYCHELLES)
-    hotels_names = test_hotel.get_hotels_names()
-
-    assert type(hotels_names) == list
-    assert len(hotels_names) > cfg.MIN_HOTELS_NUMBER
-    for i in hotels_names:
-        assert type(i) == str
-
-
-def test_get_most_expensive():
-    """Tests if the function 'most_expensive' returns a tuple, when the first element is the hotel
-        name and the second one is a number above zero (price)"""
-    # setup
-    test_hotel = BookScrap.HotelsManager(cfg.BOOKING_SEYCHELLES)
+    test_hotel = booking.HotelsManager(booking.constants["URLS"]["TESTED_URL"])
     most_expensive = test_hotel.most_expensive()
-    hotels_dict = test_hotel.get_hotels_as_dict()
-
     assert type(most_expensive) == tuple
-    assert most_expensive[cfg.TEXT] in hotels_dict.keys()
-    assert most_expensive[cfg.DIGIT] > 0
 
 
+def test_get_most_expensive_type_first_items():
+    """Tests if the function 'most_expensive' returns a tuple, when the first element (most expensive hotel)
+     is from type str"""
+    test_hotel = booking.HotelsManager(booking.constants["URLS"]["TESTED_URL"])
+    most_expensive_hotel = test_hotel.most_expensive()
+    assert type(most_expensive_hotel[0]) == str
 
 
+def test_get_most_expensive_type_second_items():
+    """Tests if the function 'most_expensive' returns a tuple, when the second element (price) is from type int"""
+    test_hotel = booking.HotelsManager(booking.constants["URLS"]["TESTED_URL"])
+    most_expensive_hotel = test_hotel.most_expensive()
+    assert type(most_expensive_hotel[1]) == numpy.int64
+
+
+def test():
+    test_get_hotels_names_df()
+    test_get_hotels_names_object_type()
+    test_get_most_expensive_tuples()
+    test_get_most_expensive_type_first_items()
+    test_get_most_expensive_type_second_items()
+
+
+test()
--- test_get_urls.py
+++ test_get_urls.py
@@ -1,26 +1,24 @@
 import pytest
 import get_urls
-import requests
 import validators
-import conf as cfg
-import scrape_requested_url as sru
+import json
+
 
 # setup
-# insert args for testing the functions
-tested_url = sru.requested_url()
-
+with open('conf.json') as config_file:
+    constants = json.load(config_file)
 
 def test_get_next_url_not_none():
     """Tests if the function get_next_url not returns a None value"""
     # setup
-    next_link = get_urls.get_next_url(tested_url)
+    next_link = get_urls.get_next_url(constants["URLS"]["TESTED_URL"])
     assert next_link  # not None
 
 
 def test_get_next_url_is_url():
     """Tests if the function get_next_url returns a valid url """
     # setup
-    next_link = get_urls.get_next_url(tested_url)
+    next_link = get_urls.get_next_url(constants["URLS"]["TESTED_URL"])
     valid = validators.url(next_link)
     assert valid
 
@@ -28,14 +26,15 @@ def test_get_next_url_is_url():
 def test_get_next_url_is_none():  # when inserting the last url
     """Tests if the function get_next_url returns None when it receives the last url as an input"""
     # setup
-    next_link = get_urls.get_next_url(get_urls.get_all_urls[-1])
+    last_link = get_urls.get_all_urls(constants["URLS"]["TESTED_URL"])[-1]
+    next_link = get_urls.get_next_url(last_link)
     assert not next_link
 
 
 def test_get_all_urls_are_urls():
     """Tests if the function get_all_urls returns a list of valid urls"""
     # setup
-    all_links = get_urls.get_all_urls(tested_url)
+    all_links = get_urls.get_all_urls(constants["URLS"]["TESTED_URL"])
     for link in all_links:
         valid = validators.url(link)
         assert valid
@@ -43,26 +42,26 @@ def test_get_all_urls_are_urls():
 
 def test_get_all_urls_list_type():
     # setup
-    all_links = get_urls.get_all_urls(tested_url)
+    all_links = get_urls.get_all_urls(constants["URLS"]["TESTED_URL"])
     assert type(all_links) == list
 
 
 def test_get_all_urls_not_empty():
     # setup
-    all_links = get_urls.get_all_urls(tested_url)
+    all_links = get_urls.get_all_urls(constants["URLS"]["TESTED_URL"])
     assert len(all_links) >= 1
 
 
 def test_last_page_number_int_type():
     """tests if the function last_page_number returns an int"""
     # setup
-    last_link = get_urls.last_page_number(tested_url)
+    last_link = get_urls.last_page_number(constants["URLS"]["TESTED_URL"])
     assert type(last_link) == int
 
 
 def test_last_page_number_reliability():
     """tests if the function last_page_number is equal to the length of the list get_all_urls returns"""
-    all_links = get_urls.get_all_urls(tested_url)
-    last_link_number = get_urls.last_page_number(tested_url)
+    all_links = get_urls.get_all_urls(constants["URLS"]["TESTED_URL"])
+    last_link_number = get_urls.last_page_number(constants["URLS"]["TESTED_URL"])
     assert len(all_links) == last_link_number
 
