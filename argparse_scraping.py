06820dbc008fd5902f3daf394150563226aeee15
Hadar Brayer <hadarbrayer@gmail.com>
2021-05-09 08:56:20 +0300

    argparse and conf update

--- argparse_scraping.py
+++ argparse_scraping.py
@@ -1,6 +1,11 @@
 import argparse
 from datetime import date, timedelta
+import json
+import sys
+import logging_file as log_f
 
+with open('conf.json') as config_file:
+    constants = json.load(config_file)
 
 def valid_date(d):
     try:
@@ -16,20 +21,25 @@ def valid_date(d):
 
 def args_parse():
     parser = argparse.ArgumentParser(description='hotels filter')
-    parser.add_argument('db_name', help="database name", type=str)
-    parser.add_argument('destination', help='destination', type=str)
-    parser.add_argument('checkin', help='date in ISO format', type=valid_date)
-    parser.add_argument('checkout', help='date in ISO format', type=valid_date)
-    parser.add_argument('--adults', help='number of adults', type=int, default=2)
-    parser.add_argument('--children', help="number of children", default=0)
-    parser.add_argument('--rooms', help='number of rooms', type=int, default=1)
+    parser.add_argument('db_name', help="database name of the current search result", type=str)
+    parser.add_argument('destination', help='Trip destination', type=str)
+    parser.add_argument('checkin', help='check-in date at the hotel in ISO format (yyyy-mm-dd)', type=valid_date)
+    parser.add_argument('checkout', help='check-out date at the hotel in ISO format (yyyy-mm-dd)', type=valid_date)
+    parser.add_argument('--adults', help='number of adults in the reservation', type=int, default=2)
+    parser.add_argument('--children', help="number of children in the reservation", default=0)
+    parser.add_argument('--rooms', help='number of rooms required (Should not be higher than number of adults!)', type=int, default=1)
 
-    parser.add_argument('--host', help="mysql host", type=str, default="localhost")
-    parser.add_argument('--user', help="mysql user", type=str, default="root")
-    parser.add_argument('--password', help="mysql password", type=str, default="root")
+    parser.add_argument('--host', help="mysql host", type=str, default=constants["SQL"]["HOST"])
+    parser.add_argument('--user', help="mysql user", type=str, default=constants["SQL"]["USER"])
+    parser.add_argument('--password', help="mysql password", type=str, default=constants["SQL"]["PASSWORD"])
 
 
     args = parser.parse_args()
     if args.checkin > args.checkout:
         args.checkout = args.checkin + timedelta(days=1)  # if checkout < checkin, calculate one night only
+
+    if args.rooms > args.adults:
+        log_f.logger.critical('Number of rooms can not be higher than number of adults!')
+        sys.exit(1)
+
     return args
